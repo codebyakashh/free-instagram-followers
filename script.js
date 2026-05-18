e462f4508796e6f97146907ece8094bcd81a2fcb
Akash Chaudhary <akashhemraj2008@gmail.com>
2026-05-18 20:33:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+const form = document.getElementById("registerForm");
+
+form.addEventListener("submit", async (e) => {
+
+  e.preventDefault();
+
+  const data = {
+
+    name: document.getElementById("name").value,
+
+    email: document.getElementById("email").value,
+
+    password: document.getElementById("password").value
+
+  };
+
+  try {
+
+    const response = await fetch(
+      "http://localhost:5000/api/auth/register",
+      {
+
+        method: "POST",
+
+        headers: {
+          "Content-Type": "application/json"
+        },
+
+        body: JSON.stringify(data)
+
+      }
+    );
+
+    const result = await response.json();
+
+    alert(result.message);
+
+  } catch (error) {
+
+    console.log(error);
+
+    alert("Server Error");
+
+  }
+  async function fetchUsers() {
+
+  try {
+    
+    const response = await fetch(`http://127.0.0.1:5000/api/auth/check-profile?username=${username}`)
+  
+
+    const users = await response.json();
+
+    const usersDiv = document.getElementById("users");
+
+    usersDiv.innerHTML = "";
+
+    users.forEach((user) => {
+
+      usersDiv.innerHTML += `
+
+        <div class="card">
+
+          <h3>${user.name}</h3>
+
+          <p>${user.email}</p>
+
+        </div>
+
+      `;
+
+    });
+
+  } catch (error) {
+
+    console.log(error);
+
+  }
+
+}
+
+fetchUsers();
+
+});
